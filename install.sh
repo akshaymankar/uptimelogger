0172c8a64aa4d38ba860423bb7efae5e381febff
Akshay Mankar <itsakshaymankar@gmail.com>
2012-02-05 03:25:13 +0530

    logging all uptimes in /var/log/uptime.log and user check done using id -u

--- install.sh
+++ install.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
-user=$(whoami)
-if [  "$user" != "root" ];
+if [ "$(id -u)" != "0" ];
 then
 	echo "You must be root to install uptimelogger"
-	echo $user; 
 	exit 1
 else
-	echo "SHELL=/bin/sh\
-	PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\
-	* * * * * root uptime > /var/log/uptime" > /etc/cron.d/uptimelogger
-
-	echo "#! /bin/sh\
-	PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\
-	mv /var/log/uptime /var/log/uptime.1" > /etc/init.d/uptimelogger
-
-	ln -s /etc/init.d/uptimelogger /etc/rcS.d/S99uptimelogger;
+	echo "SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+@reboot root cat /var/log/uptime >> /var/log/uptime.log
+* * * * * root uptime > /var/log/uptime" > /etc/cron.d/uptimelogger
 fi
 
 
